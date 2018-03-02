37fd7bcc019292ae0ce2a93561194da37aee0876
fentonca <fentonca@labprod.tsl.telus.com>
2018-03-02 03:51:16 -0500

    Added all files

---
+++ app/src/main/java/com/cameronfenton/tinderswipe/Data.java
@@ -0,0 +1,22 @@
+package com.cameronfenton.tinderswipe;
+
+public class Data {
+
+    private String description;
+
+    private String imagePath;
+
+    public Data(String imagePath, String description) {
+        this.imagePath = imagePath;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+}
---
+++ app/src/main/java/com/cameronfenton/tinderswipe/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.cameronfenton.tinderswipe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cameronfenton.tinderswipe.R;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new CountDownTimer(3000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                //nothing
+            }
+
+            public void onFinish() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }.start();
+    }
+
+
+}
