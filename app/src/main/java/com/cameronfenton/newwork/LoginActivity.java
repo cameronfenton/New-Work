0baef24c2db7f993e698cbfc7a7e2f5ce9346c28
Cameron Fenton <fentoncameron@gmail.com>
2021-08-27 10:29:49 -0400

    Added Database connection for login and register functionality, ui, libs, etc.

--- app/src/main/java/com/cameronfenton/newwork/LoginActivity.java
+++ app/src/main/java/com/cameronfenton/newwork/LoginActivity.java
@@ -2,10 +2,18 @@ package com.cameronfenton.newwork;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
  * A login screen that offers login via email/password.
@@ -18,6 +26,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         inputUsername = (EditText) findViewById(R.id.txtEmail);
         inputPassword = (EditText) findViewById(R.id.txtPassword);
@@ -28,14 +38,45 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                String username = inputUsername.getText().toString();
+                String enteredPassword = inputPassword.getText().toString();
+                String password = "";
+                boolean passwordResult = false;
+
+                try {
+
+                    Connection conn = getPostgreSQLConnection();
+
+                    Statement st = conn.createStatement();
+                    ResultSet rs = st.executeQuery("SELECT * FROM users WHERE email='"+username+"'");
+                    while (rs.next()) {
+                        System.out.print("Column 1 returned ");
+                        System.out.println("Password" + rs.getString("password_digest"));
+                        password=rs.getString("password_digest");
+                    }
+
+                    passwordResult = BCrypt.checkpw(enteredPassword, password);
+
+                    System.out.println("Password result " + passwordResult);
+
+                    rs.close();
+                    st.close();
+                    conn.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+                if (passwordResult) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                    System.out.println("Login successful");
+                }
             }
         });
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
@@ -45,5 +86,25 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
-}
 
+    public Connection getPostgreSQLConnection() {
+        Connection conn = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            String mysqlConnUrl = "jdbc:postgresql://99.238.34.94:5432/newwork_production";
+
+            String mysqlUserName = "newwork";
+
+            String mysqlPassword = "password";
+
+            conn = DriverManager.getConnection(mysqlConnUrl, mysqlUserName , mysqlPassword);
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            return conn;
+        }
+    }
+}
--- app/src/main/java/com/cameronfenton/newwork/RegisterActivity.java
+++ app/src/main/java/com/cameronfenton/newwork/RegisterActivity.java
@@ -1,16 +1,27 @@
 package com.cameronfenton.newwork;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  * A register screen that offers login via email/password.
  */
 public class RegisterActivity extends AppCompatActivity {
     private Button btnRegister;
-    private EditText inputUsername, inputPassword, inputPassword2;
+    private EditText inputUsername, inputFirstName, inputLastName, inputPassword, inputPassword2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +29,83 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         inputUsername = (EditText) findViewById(R.id.txtEmail);
+        inputFirstName = (EditText) findViewById(R.id.txtFirstName);
+        inputLastName = (EditText) findViewById(R.id.txtLastName);
         inputPassword = (EditText) findViewById(R.id.txtPassword);
-        inputPassword2 = (EditText) findViewById(R.id.txtPassword2);
+        inputPassword2 = (EditText) findViewById(R.id.txtPassword1);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password;
+                String fname = inputFirstName.getText().toString();
+                String lname = inputLastName.getText().toString();
+                String email = inputUsername.getText().toString();
+                String password1 = inputPassword.getText().toString();
+                String password2 = inputPassword.getText().toString();
+                Date date = new java.util.Date();
+                Timestamp currDateTime = new Timestamp(date.getTime());
+
+                System.out.println("Passwords: " + password1 +" " + password2);
+
+                if (password1.equals(password2)) {
+
+                    password = password1;
+
+                    try {
+                        System.out.println("passwords are equal attempting to insert");
+                        Connection conn = getPostgreSQLConnection();
+
+                        Statement st = conn.createStatement();
+                        ResultSet rs = st.executeQuery("select count(*) from users");
+                        rs.next();
+                        int tblI = rs.getInt(1)+1;
+
+                        String query = "INSERT INTO users VALUES ('" + tblI + "', '" + fname + "','" + lname + "', '" + password + "', '" + email + "', '" + currDateTime +"', '" + currDateTime +"')";
+                        System.out.println("Query: " + query + "");
+
+                        int count = st.executeUpdate(query);
+
+                        if (count > 0) {
+                            System.out.println("Successful");
+                        } else {
+                            System.out.println("Not Successful");
+                        }
+
+                        st.close();
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
 
     }
+
+
+    public Connection getPostgreSQLConnection() {
+        Connection conn = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            String mysqlConnUrl = "jdbc:postgresql://99.238.34.94:5432/newwork_production";
+
+            String mysqlUserName = "newwork";
+
+            String mysqlPassword = "password";
+
+            conn = DriverManager.getConnection(mysqlConnUrl, mysqlUserName , mysqlPassword);
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            return conn;
+        }
+    }
+
+
 }
 
